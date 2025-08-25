50d7ec2c8c5547f62abd1ad1c0f8a75e287783bb
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-08-25 16:00:28 -0300

    E, F, G

---
+++ Java_Iniciante/G_Arrays_Vetores_Matrizes/X_Matrizes.java
@@ -0,0 +1,10 @@
+package Java_Iniciante.G_Arrays_Vetores_Matrizes;
+
+public class X_Matrizes {
+    
+    public static void main(String[] args) {
+        
+        // Matrizes
+        
+    }
+}
