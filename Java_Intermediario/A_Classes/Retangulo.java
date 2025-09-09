5120817872b78c200f28a21b3b1eda058874dea1
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-09-09 15:55:25 -0300

    Classes A

---
+++ Java_Intermediario/A_Classes/Retangulo.java
@@ -0,0 +1,11 @@
+package Java_Intermediario.A_Classes;
+
+public class Retangulo {
+
+    double base = 10;   // Atributo
+    double altura = 10; // Atributo
+
+    public double area(){   // Método
+        return base * altura;
+    }
+}
---
+++ Java_Intermediario/A_Classes/index.java
@@ -0,0 +1,11 @@
+package Java_Intermediario.A_Classes;
+
+public class index {
+
+    public static void main(String[] args) {
+        
+        Retangulo r1 = new Retangulo();  // Instanciar (r1 é o nosso objeto)
+        System.out.println("Área: " + r1.area());   // Acessando método da classe Retangulo
+
+    }
+}
